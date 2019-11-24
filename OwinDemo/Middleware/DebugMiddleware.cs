099f094bf76487498842372f81b3e1a89680d500
Marco Carcini <marco.carcini@softtek.com>
2019-11-23 20:45:52 -0600

    Add a middleware class

---
+++ OwinDemo/Middleware/DebugMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using AppFunc = System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>;
+
+namespace OwinDemo.Middleware
+{
+    public class DebugMiddleware
+    {
+        AppFunc _next;
+        public DebugMiddleware(AppFunc next) {
+            _next = next;
+        }
+
+        public async Task Invoke(IDictionary<string, object> environment) {
+            var ctx = new OwinContext(environment);
+
+            Debug.WriteLine("Incoming Request: " + ctx.Request.Path);
+            await _next(environment);
+            Debug.WriteLine("Outgoing Request: " + ctx.Request.Path);
+        }
+
+    }
+}
